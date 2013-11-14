564b63b621dbeb2f15b2dfc7df9d58c64a9ff1df
A Mattson <alex.mattson@gmail.com>
2013-11-14 02:57:26 -0600

    need to finish quotes

--- src/DBApp.java
+++ src/DBApp.java
@@ -98,6 +98,7 @@ public class DBApp {
 				userDouble = Double.parseDouble(userString);
 				uuidString = getUUID();
 				System.out.println("Inserting the following values: " + uuidString + " ... " + userNameInput + " ... " + userDouble);
+				insertValue(uuidString, userNameInput, userDouble);
 				
 			}
 			
@@ -188,6 +189,55 @@ public class DBApp {
 	{
 		String uuid = UUID.randomUUID().toString();
 		return uuid;
+	}
+	
+	public static void insertValue (String uuid, String name, double balance)
+	{
+		Connection conn = null;
+		Statement stmt = null;
+		
+		try{
+			//STEP 2: Register JDBC driver
+		    Class.forName("com.mysql.jdbc.Driver");
+
+		    //STEP 3: Open a connection
+		    System.out.println("Connecting to database...");
+		    conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+		    //STEP 4: Execute a query
+		    System.out.println("Creating statement...");
+		    stmt = conn.createStatement();
+		    String sql;
+		    sql = "INSERT INTO table1 " +
+	                   "VALUES (" + uuid + "," + name + "," + balance + ")";
+		    stmt.executeUpdate(sql);
+		    // System.out.println(sql);
+		
+
+		      //STEP 5: Clean-up environment
+
+		      stmt.close();
+		      conn.close();
+		   }catch(SQLException se){
+		      //Handle errors for JDBC
+		      se.printStackTrace();
+		   }catch(Exception e){
+		      //Handle errors for Class.forName
+		      e.printStackTrace();
+		   }finally{
+		      //finally block used to close resources
+		      try{
+		         if(stmt!=null)
+		            stmt.close();
+		      }catch(SQLException se2){
+		      }// nothing we can do
+		      try{
+		         if(conn!=null)
+		            conn.close();
+		      }catch(SQLException se){
+		         se.printStackTrace();
+		      }//end finally try
+		   }//end try
 	}
 	
 }
